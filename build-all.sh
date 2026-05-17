6536ae596b2c1ec50dc4bc6f8ac889d58b53df42
William "Amos" Confer <conferw@sunypoly.edu>
2026-05-16 21:06:07 -0400

    Get PLATFORM in build scripts working for macOS and some Linux
    The root and individual example 'build-all' scripts were no longer
    working with modern macOS because darwin24.4.0 on these are not 'darwin'
    but rather start with 'darwin'... eg, 'darwin24.4.0'.  Some Linux
    and MinGW environments have a similar problem.  To resolve this,
    PLATFORM was changed from an associateive array to a variable so
    globbing conditions could be used to set PLATFORM to the appropriate
    build directory name based on what darwin24.4.0 starts with.

--- build-all.sh
+++ build-all.sh
@@ -1,16 +1,27 @@
-# maps OS string to folder name
-declare -A PLATFORM
 # finds every directory named proc
 declare -A PROCS=$(find . -name 'proc')
 # grabs script directory
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-PLATFORM["msys"]="win"
-PLATFORM["darwin"]="osx"
-PLATFORM["linux-gnu"]="linux"
+# Build scripts are separated into directories by operating system.
+# The PLATFORM string identifies the correct directory for this system,
+# expected in the "proc" directory of each example.
+#
+# The *s in the pattern matching handle common variations in values
+# for $OSTYPE, such as "darwin24.4.0" and "linux" vs "linux-gnu".
+case "$OSTYPE" in
+    msys*|mingw*) PLATFORM="win"   ;;
+    linux*)       PLATFORM="linux" ;;
+    darwin*)      PLATFORM="osx"   ;;
+    *)
+        echo "Unknown operating system type: $OSTYPE"
+        exit 1
+        ;;
+esac
+
 for dir in ${PROCS[@]}; do
     echo ${dir:2:-4}
     cd ${dir::-4}
-    bash proc/${PLATFORM["$OSTYPE"]}/*.sh
+    bash proc/${PLATFORM}/*.sh
     cd ${SCRIPT_DIR}
 done
--- ex_core_graphics/build-all.sh
+++ ex_core_graphics/build-all.sh
@@ -1,11 +1,22 @@
-declare -A PLATFORM
-# maps OS string to folder name
-PLATFORM["msys"]="win"
-PLATFORM["darwin"]="osx"
-PLATFORM["linux-gnu"]="linux"
+# Build scripts are separated into directories by operating system.
+# The PLATFORM string identifies the correct directory for this system,
+# expected in the "proc" directory of each example.
+#
+# The *s in the pattern matching handle common variations in values
+# for $OSTYPE, such as "darwin24.4.0" and "linux" vs "linux-gnu".
+case "$OSTYPE" in
+    msys*|mingw*) PLATFORM="win"   ;;
+    linux*)       PLATFORM="linux" ;;
+    darwin*)      PLATFORM="osx"   ;;
+    *)
+        echo "Unknown operating system type: $OSTYPE"
+        exit 1
+        ;;
+esac
+
 for d in */ ; do
     echo $d
     cd $d
-    bash proc/${PLATFORM["$OSTYPE"]}/*.sh
+    bash proc/${PLATFORM}/*.sh
     cd ..
 done
